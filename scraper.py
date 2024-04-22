ceb9f9a56b0cfc905c1e06065eb11696bcefda62
ft-scobra <stansmail02@gmail.com>
2024-04-22 10:56:54 -0700

    modified:   .gitignore 	modified:   scraper.py 	new file:   webpages/characters/.gitkeep 	new file:   webpages/lists/.gitkeep 	new file:   webpages/nations/.gitkeep 	new file:   webpages/npcs/.gitkeep

--- scraper.py
+++ scraper.py
@@ -25,22 +25,33 @@ def get_char_list(url):
 
 def scraper(scrape):
     if scrape == True:
-        webpages = {
-            "Character_List": "https://genshin-impact.fandom.com/wiki/Character/List",
-            "Nation_List": "https://genshin-impact.fandom.com/wiki/Category:Nations"
-        }
-        characters = get_char_list(webpages["Character_List"])
-        nations = [nation for nation in get_nation_list(
-            webpages["Nation_List"])]
-        print(nations)
-        for page in characters[0] + nations:
+        list_webpages = {"Character_List": "https://genshin-impact.fandom.com/wiki/Character/List",
+                         "Nation_List": "https://genshin-impact.fandom.com/wiki/Category:Nations",
+                         "NPC_List": "https://genshin-impact.fandom.com/wiki/NPC/List"}
+        webpages = {}
+        characters = get_char_list(list_webpages["Character_List"])
+        nations = get_nation_list(list_webpages["Nation_List"])
+        npcs = get_npc_list(list_webpages["NPC_List"])
+        for page in characters[0] + nations + npcs:
             page = page.replace(" ", "_")
+            page = page.replace('"', "'")
             webpages[page] = "https://genshin-impact.fandom.com/wiki/%s" % page
         print("Scraping")
+        for filename in list_webpages:
+            data = list_webpages[filename]
+            with open("webpages/lists/%s.html" % filename, "w+", encoding="utf-8") as f:
+                f.write(str(soup(data)))
         for filename in webpages:
             data = webpages[filename]
-            with open("webpages/%s.html" % filename, "w+", encoding="utf-8") as f:
-                f.write(str(soup(data)))
+            if filename in nations:
+                with open("webpages/nations/%s.html" % filename, "w+", encoding="utf-8") as f:
+                    f.write(str(soup(data)))
+            elif filename in characters[0]:
+                with open("webpages/characters/%s.html" % filename, "w+", encoding="utf-8") as f:
+                    f.write(str(soup(data)))
+            elif filename in npcs:
+                with open("webpages/npcs/%s.html" % filename, "w+", encoding="utf-8") as f:
+                    f.write(str(soup(data)))
 
 
 def get_nation_list(webpage):
@@ -48,6 +59,17 @@ def get_nation_list(webpage):
     nations = [nation.text.strip() for nation in webpage.select(
         ".category-page__member-link")]
     return (nations)
+
+
+def get_npc_list(webpage):
+    webpage = soup(webpage)
+    npc_list = list({npc.text.strip()
+                     for npc in webpage.select(".columntemplate a")})
+    print("===")
+    print("Number of NPC's: ", len(npc_list))
+    npc_list.sort()
+    print("===")
+    return (npc_list)
 
 
 def get_related_characters(character_list, table_number, nation, webpage, character_nations):
@@ -63,7 +85,7 @@ def get_related_characters(character_list, table_number, nation, webpage, charac
 def get_nation_characters(characters_list, nations_list):
     character_nations = {nation: [] for nation in nations_list}
     for nation in nations_list:
-        webpage = soup("webpages/%s.html" % nation)
+        webpage = soup("webpages/nations/%s.html" % nation)
         get_related_characters(characters_list, 1, nation,
                                webpage, character_nations)
         get_related_characters(characters_list, 2, nation,
@@ -82,9 +104,9 @@ print("===")
 print("Scrape: ", should_scrape)
 print("===")
 
-scraper(should_scrape)
+# scraper(should_scrape)
 
-nations = get_nation_list("webpages/Nation_List.html")
+nations = get_nation_list("webpages/lists/Nation_List.html")
 
 print("List of nations:", nations)
 print("===")
@@ -93,6 +115,8 @@ character_nations = get_nation_characters(characters, nations)
 
 print("Characters related to each nation:")
 print(character_nations)
+
+npcs = get_npc_list("webpages/lists/NPC_List.html")
 
 
 list_of_nodes = [characters, nations]
